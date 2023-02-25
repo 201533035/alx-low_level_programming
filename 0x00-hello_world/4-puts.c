85b34f94852bc89f415f8c56d4c3d3742075be4b
201533035 <jotheromohlawe@gmail.com>
2023-02-24 18:59:36 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - program that prints a line with puts function
  * return: 0 (success)
  */
-int main (void)
-{ puts ("\"Programming is like building a multilingual puzzle");
+int main(void)
+{ puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
