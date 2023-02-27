011d46e8e0591170c6cfd1a7a1cdead78816a1f4
201533035 <jotheromohlawe@gmail.com>
2023-02-27 15:30:06 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		put_char(43);
+		_putchar(43);
 		return (1);
 	}
 	else if (n < 0)
 	{
-		 put_char(45);
+		 _putchar(45);
                  return (-1);
 	}
 	else
 	{
-		 put_char(40);
+		 _putchar(40);
                  return (0);
 	}
 }
